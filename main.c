2c19a6b7321cf55cff0e00abaea0cdb37cea08fa
Balaji_Pandiyan <balajibj42@gmail.com>
2025-09-13 13:09:07 +0530

    SET_BIT updated

--- main.c
+++ main.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define SET_BIT(x, y) ((x) |= (1<<y))
+
 int main(void)
 {
     printf("START: main\n");
+    int x = 0 , y = 0;
     printf("Application Code Here_00\n");
     printf("Application Code Here_01\n");
+    printf("SET_BIT: %d\n",SET_BIT(x, y));
     printf("END: main\n");
     return EXIT_SUCCESS;
 }
