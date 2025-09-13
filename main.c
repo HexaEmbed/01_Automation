299ff44cb624ffe12bdad35ca51d596a37c42b9a
Balaji_Pandiyan <balajibj42@gmail.com>
2025-09-13 12:05:04 +0530

    Getting Started Test Automation

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    printf("START: main\n");
+    printf("Application Code Here..\n");
+    printf("END: main\n");
+    return EXIT_SUCCESS;
+}
