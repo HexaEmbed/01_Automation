9b2a23b34af57139e4e7a9855af50088a8bf0164
Balaji_Pandiyan <balajibj42@gmail.com>
2025-09-13 13:31:15 +0530

    CLR_BIT

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 
 #define SET_BIT(x, y) ((x) |= (1<<y))
+#define CLR_BIT(x, y) ((x) &= ~(1<<y))
 
 int main(void)
 {
@@ -10,7 +11,7 @@ int main(void)
     printf("Application Code Here_00\n");
     printf("Application Code Here_01\n");
     printf("SET_BIT: %d\n",SET_BIT(x, y));
-    printf("SET_BIT: %d\n",SET_BIT(x, y));
+    printf("CLR_BIT: %d\n",CLR_BIT(x, y));
     printf("END: main\n");
     return EXIT_SUCCESS;
 }
