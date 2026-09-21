f724f17517aaf30c992c6e82a69b6b670e777997
HexaEmbed <HexaEmbed@gmail.com>
2026-09-21 11:20:16 +0100

    test

--- main.c
+++ main.c
@@ -14,7 +14,7 @@ int main(void)
     printf("SET_BIT: %d\n",SET_BIT(x, y));
     printf("CLR_BIT: %d\n",CLR_BIT(x, y));
     printf("TGL_BIT: %d\n",TGL_BIT(x, y));
-    x = 5; y = 1;
+    x = 6; y = 1;
     printf("SET_BIT: %d\n",SET_BIT(x, y));
     printf("CLR_BIT: %d\n",CLR_BIT(x, y));
     printf("TGL_BIT: %d\n",TGL_BIT(x, y));
